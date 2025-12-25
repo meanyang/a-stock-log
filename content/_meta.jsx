1448168f2d50b404a595f682f5e96a0d00a16466
yang <meanyang7@hotmail.com>
2025-12-25 22:38:31 +0800

    Add 12.25

---
+++ content/_meta.jsx
@@ -0,0 +1,21 @@
+export default {
+  index: '首页',
+  'dragon-tiger-list': {
+    title: '龙虎榜'
+  },
+  'knowledge-base': {
+    title: '知识库'
+  },
+  'my-concepts': {
+    title: '我的概念'
+  },
+  'my-strategies': {
+    title: '我的策略'
+  },
+  contact: {
+    title: '联系我们'
+  },
+  about: {
+    title: '关于我们'
+  }
+}
---
+++ content/about/_meta.jsx
@@ -0,0 +1,4 @@
+export default {
+  index: '关于我们',
+  legal: '免责声明'
+}
