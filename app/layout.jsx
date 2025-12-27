dd617447fa359614a33e395d4a688bdeb413a9d0
yang <meanyang7@hotmail.com>
2025-12-27 22:19:47 +0800

    Update layout and add 2025122-26.mdx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,26 @@ import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
  
 export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+  // 网站基本信息
+  title: 'AStockLog - 个人股票投资知识管理系统',
+  description: '一个基于Next.js和Nextra的个人股票投资知识管理与日志系统',
+  // 添加favicon配置
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: '48x48', type: 'image/x-icon' },
+    ],
+    shortcut: ['/favicon.ico'],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+      { rel: 'android-chrome', sizes: '192x192', url: '/android-chrome-192x192.png' },
+      { rel: 'android-chrome', sizes: '512x512', url: '/android-chrome-512x512.png' },
+    ],
+  },
 }
  
 const banner = <Banner storageKey="some-key">AStockLog is released 🎉</Banner>
@@ -32,16 +50,22 @@ export default async function RootLayout({ children }) {
   return (
     <html
       // Not required, but good for SEO
-      lang="en"
+      lang="zh-CN"
       // Required to be set
       dir="ltr"
       // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
       suppressHydrationWarning
     >
-      <Head
-      // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      <Head>
+        {/* 添加favicon链接标签 */}
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="android-chrome" href="/android-chrome-192x192.png" sizes="192x192" />
+        <link rel="android-chrome" href="/android-chrome-512x512.png" sizes="512x512" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Layout
